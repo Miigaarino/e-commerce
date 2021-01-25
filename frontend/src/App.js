91942d20428c8a65c1f38f48bc98898ea7c7085a
Myagmarsuren <miner_miga@yahoo.com>
2021-01-26 01:09:45 +0800

    Home Screen finished

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,15 @@
 import { Container } from 'react-bootstrap'
 import Header from './components/Header'
 import Footer from './components/Footer'
+import HomeScreen from './screens/HomeScreen'
 
 function App() {
   return (
     <>
       <Header />
-      <main>
+      <main className='py-3'>
         <Container>
-          <h1>Welcome to e-commerce website</h1>
+          <HomeScreen />
         </Container>
       </main>
       <Footer />
