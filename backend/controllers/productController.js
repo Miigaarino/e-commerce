531952d1fdf3aa1a5bfeba86891957731db67c20
Myagmarsuren <miner_miga@yahoo.com>
2021-02-13 17:19:20 +0800

    Top rated items carousel added

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -150,6 +150,14 @@ const createProductReview = AsyncHandler(async (req, res) => {
   }
 })
 
+// @desc get top rated products
+// @route GET api/products/top
+// @access Public
+const getTopProducts = AsyncHandler(async (req, res) => {
+  const products = await Product.find({}).sort({ rating: -1 }).limit(3)
+  res.json(products)
+})
+
 export {
   getProducts,
   getProductById,
@@ -157,4 +165,5 @@ export {
   createProduct,
   updateProduct,
   createProductReview,
+  getTopProducts,
 }
